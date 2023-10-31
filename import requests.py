b6a086cc26fc04629c26957d1c8df081cede5527
Krypt0 <99403155+Krypt0o@users.noreply.github.com>
2023-10-31 14:36:17 +0100

    Delete import requests.py

--- import requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-import access_token
-
-header = {"Client-ID":access_token.CLIENT_ID, "Authorization":f"bearer {access_token.get_access_token()}"}
-
-
-requests.delete("https://api.twitch.tv/helix/eventsub/subscriptions?id=5f8ce72d-a7c6-42bd-9451-061b602293e3", headers=header)
