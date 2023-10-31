5a699b92fc1f828c6d8a604101b03a16c6d26b39
Krypt0 <99403155+Krypt0o@users.noreply.github.com>
2023-10-31 14:36:46 +0100

    Delete me.ps1

--- me.ps1
+++
@@ -1,6 +0,0 @@
-﻿$fileToCheck = "C:\Twitch_Rec\me.mkv"
-$date=(get-date -Format d) -replace("/")
-$time=(get-date -Format t) -replace(":")
-$newfile = "me"+"_"+"$date"+"_"+"$time"+".mkv"
-streamlink -o me.mkv twitch.tv/Kry_pt0 best
-Rename-Item "C:\Twitch_Rec\me.mkv" -NewName $newfile
