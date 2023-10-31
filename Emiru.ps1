564c4148f7f2e9e5475018226307b77d7ecf4aac
Krypt0 <99403155+Krypt0o@users.noreply.github.com>
2023-10-31 14:35:27 +0100

    Delete Emiru.ps1

--- Emiru.ps1
+++
@@ -1,6 +0,0 @@
-﻿$fileToCheck = "C:\Twitch_Rec\Emiru.mkv"
-$date=(get-date -Format d) -replace("/")
-$time=(get-date -Format t) -replace(":")
-$newfile = "Emiru"+"_"+"$date"+"_"+"$time"+".mkv"
-streamlink -o Emiru.mkv twitch.tv/Emiru best
-Rename-Item "C:\Twitch_Rec\Emiru.mkv" -NewName $newfile
